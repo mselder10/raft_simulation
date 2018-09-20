70aea8ca96f5f25f283074830da19d0bc270ad03
Kevin Zheng <kzz3@duke.edu>
2018-09-20 03:41:58 -0400

    e

--- p0/dmm.c
+++ p0/dmm.c
@@ -3,9 +3,6 @@
 #include <assert.h> // needed for asserts
 #include "dmm.h"
 
-/* You can improve the below metadata structure using the concepts from Bryant
- * and OHallaron book (chapter 9).
- */
 
 typedef struct metadata {
   /* size_t is the return type of the sizeof operator. Since the size of an
@@ -14,7 +11,8 @@ typedef struct metadata {
    * implementation. size contains the size of the data object or the number of
    * free bytes
    */
-  size_t size;
+
+  size_t size; 
   struct metadata* next;
   struct metadata* prev; 
 } metadata_t;
@@ -27,21 +25,130 @@ static metadata_t* freelist = NULL;
 
 void* dmalloc(size_t numbytes) {
   /* initialize through mmap call first time */
-  if(freelist == NULL) { 			
-    if(!dmalloc_init())
-      return NULL;
-  }
+    if(freelist == NULL) { 			
+        if(!dmalloc_init())
+        return NULL;
+    }
+    assert(numbytes > 0);
 
-  assert(numbytes > 0);
+    numbytes = ALIGN(numbytes);
+    metadata_t* block1 = freelist;
+    int tot = numbytes + sizeof(metadata_t);
 
-  /* your code here */
-	
-  return NULL;
+    void* address = NULL;
+
+    while(block1->size < tot && (block1-> next != NULL)){
+      block1 = block1 -> next;
+    } //find correct loc
+
+    if(block1-> size >= tot){
+        address = (void*)(block1) + sizeof(metadata_t);
+        if(block1 -> size == tot){
+            if(block1->next != NULL){
+                block1->next->prev = block1->prev;
+            }
+
+            if(block1->prev == NULL){
+                freelist = block1 -> next;
+            }
+            else{
+                block1->prev->next = block1->next;
+            }
+        }
+        else{
+            metadata_t* block2 = (metadata_t*)(((void*)block1) + tot);
+            block2 -> size = block1-> size - tot;
+            block1 -> size = tot;
+            if(block2 -> size < ALIGN(1) + sizeof(metadata_t)){
+                if(block1->next != NULL){
+                    block1->next->prev = block1->prev;
+                }
+                if(block1->prev == NULL){
+                    freelist = block1 -> next;
+                }
+                else{
+                    block1->prev->next = block1->next;
+                }
+                return address;
+            }
+            block2->prev = block1 -> prev;
+            block2->next = block1 -> next;
+            if(block2 -> next != NULL){
+                block2 -> next -> prev = block2;
+            }
+
+            if(block2 -> prev != NULL){
+                block2 -> prev -> next = block2;
+            }
+            else{
+                freelist = block2; //first
+            }
+            
+        }
+    block1->prev = NULL;
+    block1 -> next = NULL;
+    }
+return address;
+} 
+
+
+void coalesce(){
+    metadata_t* block1 = freelist;
+    while(block1->next != NULL){
+        metadata_t* block2 = (metadata_t*)((void*)block1 + block1->size);
+        if(block1->next == block2){
+            block1 -> size = block1 -> size + block2 -> size;
+            block1 -> next = block1 -> next -> next;
+            if(block1 -> next != NULL){
+                block1 -> next -> prev = block1;
+            }
+        }
+        else
+        {
+            block1 = block1 -> next;
+        }
+    }
 }
+
 
 void dfree(void* ptr) {
-  /* your code here */
-}
+
+    metadata_t* block0 = (metadata_t*) (ptr - sizeof(metadata_t));
+    metadata_t* block1 = freelist;
+
+    if(block1 == NULL){
+        freelist = block0;
+        block0 -> next = NULL;
+        block0 -> next = NULL;
+        return;
+    }
+
+    while(block1 <= block0){
+        if(block1-> next == NULL){
+            block1->next = block0;
+            block0-> next = NULL;
+            block0 -> prev = block1;
+            coalesce();
+            return;
+        }
+        block1 = block1 -> next;
+    }
+    if(block1->prev == NULL){ //first
+        freelist = block0;
+    }
+    else{
+        block1 -> prev -> next = block0;
+    }
+    block0 -> prev = block1 -> prev;
+    block0 -> next = block1;
+    block1 -> prev = block0;
+    coalesce();
+    return;
+}    
+    
+
+
+
 
 bool dmalloc_init() {
 
@@ -71,12 +178,12 @@ bool dmalloc_init() {
 void print_freelist() {
   metadata_t *freelist_head = freelist;
   while(freelist_head != NULL) {
-    DEBUG("\tFreelist Size:%zd, Head:%p, Prev:%p, Next:%p\t",
+    printf("\tFreelist Size:%zd, Head:%p, Prev:%p, Next:%p\t",
 	  freelist_head->size,
 	  freelist_head,
 	  freelist_head->prev,
 	  freelist_head->next);
     freelist_head = freelist_head->next;
   }
-  DEBUG("\n");
+  printf("\n");
 }
